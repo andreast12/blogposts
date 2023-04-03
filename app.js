b1d2350ede9c932d6f9d7c78f6b9e329b1eaed64
andreast12 <andreast12.id@gmail.com>
2023-04-03 21:07:58 +0700

    improve the UI

--- app.js
+++ app.js
@@ -21,10 +21,14 @@ const hbs = create({
   helpers: {
     renderEditDeleteIcon(postOwner, loggedInUser, postId) {
       if (loggedInUser && postOwner._id.toString() === loggedInUser.id) {
-        return `<a href="/posts/edit/${postId}" class="btn btn-primary">Edit</a>
-        <form action="/posts/delete/${postId}?_method=delete" method="post">
-          <input type="submit" value="Delete" class="btn btn-danger"> 
-        </form>`;
+        return `<div class="d-flex column-gap-3 mt-5">
+          <a href="/posts/edit/${postId}" class="btn btn-primary"><i class="bi bi-pencil-square me-1"></i>Edit</a>
+          <form action="/posts/delete/${postId}?_method=delete" method="post">
+            <button type="submit" class="btn btn-danger">
+              <i class="bi bi-trash me-1"></i>Delete
+            </button>
+          </form>
+        </div>`;
       }
       return "";
     },
@@ -33,6 +37,9 @@ const hbs = create({
     },
     absoluteDate(date) {
       return DateTime.fromJSDate(date).toLocaleString(DateTime.DATETIME_MED);
+    },
+    addOne(num) {
+      return num + 1;
     },
   },
 });
